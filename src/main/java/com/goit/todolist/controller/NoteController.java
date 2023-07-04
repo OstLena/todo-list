5ba07bac21656e4a5dad95679201ea6fef56aa31
lena <ostapets.olena@gmail.com>
2023-07-04 20:41:41 +0300

    init commit

--- src/main/java/com/goit/todolist/controller/NoteController.java
+++ src/main/java/com/goit/todolist/controller/NoteController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,29 +17,32 @@ public class NoteController {
 
     @GetMapping("/note/list")
     public String noteList(Model model) {
-
         model.addAttribute("notes", noteService.listAll());
-
-        return "notes"; //view
+        return "notes";
     }
 
     @PostMapping("/note/add")
     public String noteAdd(Note note) {
         noteService.add(note);
         return "redirect:/note/list";
-
-//GET /note/list - отримати список нотаток.
-// Виводиться список нотаток (title та content), кожну нотатку можна видалити або редагувати
     }
-
 
     @GetMapping("/note/delete")
     public String noteDelete(@RequestParam Long id) {
         noteService.deleteById(id);
         return "redirect:/note/list";
-        //POST/note/delete-видалити нотатку по ID.Після видалення нотатки відбувається редирект на/note/list
     }
 
+    @GetMapping("/note/edit")
+    public String noteEdit(Model model, @RequestParam Long id) {
+        model.addAttribute("note", noteService.getById(id));
+        return "note_edit";
+    }
 
+    @PostMapping("/note/edit")
+    public String noteEdit(Note note) {
+        noteService.update(note);
+        return "redirect:/note/list";
+    }
 }
 
--- src/main/java/com/goit/todolist/controller/TestController.java
+++ src/main/java/com/goit/todolist/controller/TestController.java
@@ -1,26 +1,15 @@
 package com.goit.todolist.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 @Controller
 public class TestController {
 
-//    @GetMapping("/test")
-//    public String test() {
-//        return "ok";
-//    }
-
     @GetMapping("/test")
     public String test(Model model) {
-
-//        model.addAttribute("message", name);
-
-        return "test"; //view
+        return "test";
     }
 }
