f028cc1befb0747c8435362e3fb9d8feb499aee7
lena <ostapets.olena@gmail.com>
2023-07-07 15:14:09 +0300

    application properties updated with url NoteController updated with annotation

--- src/main/java/com/goit/todolist/controller/NoteController.java
+++ src/main/java/com/goit/todolist/controller/NoteController.java
@@ -7,39 +7,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping("/note")
 @Controller
 public class NoteController {
 
     @Autowired
     private NoteService noteService;
 
-    @GetMapping("/note/list")
+    @GetMapping("/list")
     public String noteList(Model model) {
         model.addAttribute("notes", noteService.listAll());
         return "notes";
     }
 
-    @PostMapping("/note/add")
+    @PostMapping("/add")
     public String noteAdd(Note note) {
         noteService.add(note);
         return "redirect:/note/list";
     }
 
-    @GetMapping("/note/delete")
+    @GetMapping("/delete")
     public String noteDelete(@RequestParam Long id) {
         noteService.deleteById(id);
         return "redirect:/note/list";
     }
 
-    @GetMapping("/note/edit")
+    @GetMapping("/edit")
     public String noteEdit(Model model, @RequestParam Long id) {
         model.addAttribute("note", noteService.getById(id));
         return "note_edit";
     }
 
-    @PostMapping("/note/edit")
+    @PostMapping("/edit")
     public String noteEdit(Note note) {
         noteService.update(note);
         return "redirect:/note/list";
